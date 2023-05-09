e50a568e00430e692333271afc15a7e11501a0c3
oqqinawa <riyanto.oki@gmail.com>
2023-05-09 17:08:38 +0700

    Init commit

---
+++ app/components/Loader.tsx
@@ -0,0 +1,13 @@
+" use client";
+
+import PuffLoader from "react-spinners/PuffLoader";
+
+const Loader = () => {
+  return (
+    <div className="h-[70vh] flex flex-col justify-center items-center">
+      <PuffLoader size={100} color="red" />
+    </div>
+  );
+};
+
+export default Loader;
